e157f7e093266880b4d2581e91ff0af65e70c495
thias92 <66361676+thias92@users.noreply.github.com>
2020-07-05 17:29:14 +0200

    Add files via upload
    Teil 3 5-Möglichkeiten

---
+++ src/components/Five.vue
@@ -0,0 +1,24 @@
+<template>
+<v-container>
+    
+        <v-col>
+          <v-radio-group v-model="five">
+              <v-radio label="Stimme überhaupt nicht zu" name='radio' value="1"></v-radio>
+              <v-radio label="Stimme teilweise nicht zu" name="radio" value="2"></v-radio>
+              <v-radio label="Neutral" name="radio" value="3"></v-radio>
+              <v-radio label="Stimme teilweise zu" name="radio" value="4"></v-radio>
+              <v-radio label="Stimme voll zu" name="radio" value="5"></v-radio>
+          </v-radio-group>
+        </v-col>
+
+</v-container>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        five: null,
+      }
+    },
+  }
+</script>
