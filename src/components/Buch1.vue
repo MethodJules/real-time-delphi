5dad81d44e24bf9f35b4ebe20caced6836c7e97c
MarcHerbach <66572571+MarcHerbach@users.noreply.github.com>
2020-07-06 23:20:21 +0200

    Update Buch1.vue

--- src/components/Buch1.vue
+++ src/components/Buch1.vue
@@ -7,14 +7,45 @@
                 <v-img src="../assets/buch1.png" />
             </v-col>
         </v-row>
+
+        <v-row align="center" justify="center">
+            <v-col>
+               
+             
+                <v-img src="../assets/buch2.png" />
+            </v-col>
+        </v-row>
+
+        <v-row align="center" justify="center">
+            <v-col>
+               
+             
+                <v-img src="../assets/buch3.png" />
+            </v-col>
+        </v-row>
+
+         <v-row align="center" justify="center">
+            <v-col>
+               
+             
+                <v-img src="../assets/buch4.png" />
+            </v-col>
+        </v-row>
+
+
     </v-container>
+
 </template>
+
 
 <script>
 export default {
     data () {
       return {
         Buch1: null,
+        Buch2: null,
+        Buch3: null,
+        Buch4: null,
       }
     },
 }
