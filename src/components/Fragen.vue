c699ad760cbb8003031d553e43282502929382b3
thias92 <66361676+thias92@users.noreply.github.com>
2020-07-20 16:39:35 +0200

    Add files via upload

---
+++ src/components/Fragen.vue
@@ -0,0 +1,172 @@
+<template>
+<v-container>
+    <div id="1">
+        Vielen Dank,
+            <br />Für Ihre Teilnahme an der Umfrage
+            Der vorliegende Befragungsbogen bezieht sich auf den Besuch von OnlineVerkaufsplattformen, auf denen man sich über Produkte (wie z.B. Bücher)
+            informieren und diese käuflich erwerben kann.
+            Der Befragungsbogen umfasst drei Bereiche:
+            <br />1. Pre-Fragebogen
+            <br />2. Simulation Einkauf auf einer Online-Verkaufsplattform
+            <br />3. Post-Fragebogen
+            <br />Bitte lesen Sie alle Anweisungen und Aussagen aufmerksam durch. Nehmen
+            Sie sich ausreichend Zeit, um die Aufgaben überlegt zu bearbeiten. Wenn Ihnen
+            einmal die Entscheidung schwerfällt, überlegen Sie, welche Lösung noch am
+            ehesten zutrifft.
+    
+    </div>
+    <div id="2">
+        Uns interessiert Ihre ehrliche, persönliche Meinung. Die Befragung findet
+            anonym statt, es werden keine persönlichen Angaben verarbeitet.
+            <br />Einzelne
+            Fragebögen können nicht auf einzelne Personen zurückgeführt werden.
+    </div>
+        <div id="3">
+            <p>Bitte beantworten Sie die Fragen zum Hintergrund der Person</p>
+        </div>
+        <div id="4">
+            Sie möchten ein bestimmtes Buch online erwerben und besuchen dazu eine Online-Verkaufsplattform. 
+      Sie wollen sich vor dem Kauf über das
+      Buch informieren und prüfen, ob es Ihnen wirklich gefallen könnte. Hierfür lesen Sie einige 
+      Buchrezensionen von Kunden, die das Buch bereits gelesen haben.
+      Sie interessiert hauptsächlich, wie andere Kunden den Sprachstil und die 
+      Erzählperspektive im Buch bewerten. Der Sprachstil ist die Art und Weise, 
+      etwas mit Worten oder in geschriebener Form zu äußern. Unter Erzählperspektive versteht 
+      man die Sicht, aus der ein Buch erzählt wird.
+      Daher suchen Sie gezielt nach diesen Informationen in den Rezensionen, um schnell und gezielt eine 
+      Kaufentscheidung treffen zu können. Sie
+      haben wenig Zeit für das Lesen der Rezensionen und wollen schnellstmöglich diese Informationen finden.
+      Im Folgenden sehen Sie die Produktbeschreibung eines Buches und eine Auswahl an Buchrezensionen:
+      <br />1. Lesen Sie zunächst die Produktbeschreibung zum Buch aufmerksam durch.
+      <br />2. Markieren Sie schnellstmöglich in den Buchrezensionen die relevanten Textstellen, also die Textstellen, 
+      in denen der Sprachstil und/oder die
+      Erzählperspektive angesprochen werden, mit einem Textmarker oder unterstreichen Sie die Textstellen. 
+      Falls Sie aus Versehen falsche
+      Textstellen markieren, streichen Sie diese einfach durch.
+        </div>
+        <div id="5">
+            <p>Haben Sie das aufgeführte Buch schon gelesen?</p>
+        </div>
+        
+        <div id="6">
+           <p>Wielange nutzen Sie schon ein Internetfähiges Endgerät(wie Tablet,Smartphone,Laptop,Desktop-PC?</p>
+        </div>
+        <div id="7">
+            <p>Sind Sie vertraut mit dem Surfen im Internet?</p>
+        </div>
+        <div id="8">
+           <p>Wie oft kaufen Sie typeischerweise Produkte au Online-Verkaufsplattfprmen</p>
+        </div>
+        <div id="9">
+            <p>Lesen Sie Kundenproduktrezension auf Online-Verkaufsplattformen, wenn Sie planen, 
+                ein Produkt zu kaufen? (Kundenproduktrezensionen sind von Kunden verfasste Texte, 
+                in denen Kunden ihre Meinung über Produkte teilen.)</p>
+        </div>
+        <div id="10">
+            <p>Wie oft kaufen Sie typeischerweise Produkte au Online-Verkaufsplattfprmen</p>
+        </div>
+        <div id="11">
+            <p>Lesen Sie Buchrezension von anderen Kunden auf Online-Verkaufsplattformen, 
+                  wenn Sie planen, ein Buch zu kaufen? (Buchrezensionen
+                sind von Kunden verfasste Texte, in denen Kunden ihre 
+                Meinung über Bücher teilen.)</p>
+        </div>
+        <div id="12">
+            <p>Um die Zusammengehörigkeit der Seiten des Befragungsbogens 
+                identifizieren zu können und zugleich Ihre Anonymität zu schützen, ist zunächst
+                eine ID herzuleiten. Die ID setzt sich aus den Antworten a) bis d) zusammen.</p>
+        </div>
+        <div id="13">
+             <p>Während ich die Buchrezensionen betrachtet habe, konnte ich die meisten Ablenkungen ausblenden.</p>
+        </div>
+        <div id="14">
+            <p>Während ich die Buchrezensionen betrachtet habe, war ich in das, was ich tat vertieft. </p>
+        </div>
+        <div id="15">
+            <p>Während ich die Buchrezensionen betrachtet habe, war ich in die Aufgabe vertieft, 
+              die ich gerade ausführte.</p>
+        </div>
+        <div id="16">
+             <p>Als ich die Buchrezensionen betrachtet habe, ließ ich mich sehr leicht durch andere Dinge ablenken. </p>
+        </div>
+        <div id="17">
+            <p>Während ich die Buchrezensionen betrachtet habe, 
+              wurde meine Aufmerksamkeit nicht leicht gestört. </p>
+        </div>
+        <div id="18">
+            <p>Während ich die Buchrezensionen betrachtet habe, wurde ich von den Personen im Raum gestört.  </p>
+        </div>
+        <div id="19">
+           <p>Während ich die Buchrezensionen betrachtet habe, konnte ich mich aufgrund von Störungen im
+            Raum nicht auf die Aufgabe konzentrieren. </p>
+        </div>
+        <div id="20">
+            <p>Während ich die Buchrezensionen betrachtet habe, habe ich mir Mühe gegeben, 
+              die Aufgabe zu bearbeiten. </p>
+        </div>
+        <div id="21">
+             <p>Während ich die Buchrezensionen betrachtet habe, war ich motiviert, die Aufgabe zu bearbeiten. </p>
+        </div>
+        <div id="22">
+            <p>Während ich die Buchrezensionen betrachtet habe, war ich bereit, mich mit der Aufgabe zu
+            befassen. </p>
+        </div>
+        <div id="23">
+            <p>Wenn ich zukünftig auf Online-Verkaufsplattformen Bücher kaufen und mich vorab informieren
+            möchte, würde ich eine Plattform mit der hier gezeigten Darstellungsart von Rezensionen nutzen. </p>
+        </div>
+        <div id="24">
+            <p>Wenn ich zukünftig auf Online-Verkaufsplattformen Bücher kaufen und mich vorab informieren
+            möchte, wäre die Nutzung einer Plattform mit der hier gezeigten Darstellungsart von Rezensionen
+            für mich ansprechend. </p>
+        </div>
+        <div id="25">
+            <p>Die Darstellungsweise der Rezension hat dazu beigetragen, dass ich die für mich relevanten
+            Informationen ohne große Mühe auffinden konnte. </p>
+        </div>
+        <div id="26">
+          <p>Die Darstellungsweise der Rezension hat dazu beigetragen, dass es mir leicht gefallen ist, 
+              die für mich relevanten Informationen in der Rezension zu finden.</p>
+        </div>
+
+</v-container>
+</template>
+<script>
+  export default {
+    data () {
+       posts:[
+        
+      //{ id: 1, title: 'Begrüßung' }, 
+     // { id: 2, title: 'Datenschutz' },
+      //{ id: 3, title: 'Buchrezension' },
+      //{ id: 4, title: 'Gelesen' },
+     // { id: 5, title: 'Question1' },
+      //{ id: 6, title: 'Question2' },
+      //{ id: 7, title: 'Question3' },
+      //{ id: 8, title: 'Question4' },
+     // { id: 9, title: 'Question5' },
+     // { id: 10, title: 'Question6' },
+     // { id: 11, title: 'Question7' },
+      { id: 12, title: 'ID' },
+     { id: 13, title: 'Question8' },
+      { id: 14, title: 'Question9' },
+     { id: 15, title: 'Question10' },
+      { id: 16, title: 'Question11' },
+      { id: 17, title: 'Question12' },
+     { id: 18, title: 'Question13' },
+      { id: 19, title: 'Question14' },
+      { id: 20, title: 'Question15' },
+      { id: 21, title: 'Question16' },
+     { id: 22, title: 'Question17' },
+      { id: 23, title: 'Question18' },
+    { id: 24, title: 'Question19' },
+     { id: 25, title: 'Question20' },
+      { id: 26, title: 'Question21' }
+      
+    ]
+      return {
+        five: null,
+      }
+    },
+  }
+</script>
