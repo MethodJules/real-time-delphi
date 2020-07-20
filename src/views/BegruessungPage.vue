a6a75a5cdba85660af2458e690d4808c5d10aa84
Julien <hoferj@uni-hildesheim.de>
2020-07-20 19:34:36 +0200

    Begruessungs-Seite

---
+++ src/views/BegruessungPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="begruessung">
+        <div>
+            <p>{{begruessungsText.text}}</p>
+            <router-link to="/datenschutz">Weiter</router-link>
+        </div>
+    </section>
+</template>
+<script>
+import store from '@/store.js';
+export default {
+    data() {
+        return {
+            begruessungsText: store.survey[0]
+        }
+    }
+}
+</script>
