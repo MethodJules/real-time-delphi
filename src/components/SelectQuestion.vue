6b07a7b37a9d939357b63295ad48f030235b1afc
Julien <hoferj@uni-hildesheim.de>
2020-07-20 19:34:14 +0200

    SelectQuestion-Komponente

---
+++ src/components/SelectQuestion.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-container>
+        <v-col>
+            <p>{{question}}</p>
+            <v-radio-group v-model="selection">
+                <v-radio @change="logAnswer" v-for="answer in answers" :key="answer.label" :label=answer.label />
+            </v-radio-group>
+        </v-col>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            //question: "Testfrage",
+            selection: 0,
+            /*
+            answers: [
+                {
+                    label: "0 Jahre"
+                },
+                {
+                    label: "< 1 Jahr"
+                },
+                {
+                    label: "1-2 Jahre"
+                }
+            ],*/
+        }       
+    },
+    props: {
+        question: String,
+        answers: Array
+    },
+    methods: {
+        logAnswer() {
+            console.log(this.selection)
+        }
+    }
+}
+</script>
