b4fc2f85d1e6dfadd86be8ce04b727ac053655fc
thias92 <66361676+thias92@users.noreply.github.com>
2020-10-12 16:57:39 +0200

    Delete MarkerQuestion.vue

--- src/components/MarkerQuestion.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <v-container>
-        <v-col>
-            <h2>Buchrezensions zum Buch: </h2>
-            <p>Bitte markieren Sie schnellstmöglich die Textstellen im Rezensionstext, welche den Sprachstil und/oder die Erzählperspektive thematisieren.</p>
-            
-        </v-col>
-    </v-container>
-</template>
